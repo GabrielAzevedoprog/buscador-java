c839205e0a6dd898a423bedc8de281887d766a5d
GabrielAzevedoprog <142434640+GabrielAzevedoprog@users.noreply.github.com>
2025-07-24 23:21:02 -0300

    Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+class Principal {
+    public static void main(String[] args) {
+
+        ConsultaCep consultaCep = new ConsultaCep();
+        Endereco novoEndereco = consultaCep.buscaEndereco("01001000");
+        System.out.println(novoEndereco);
+
+    }
+}
